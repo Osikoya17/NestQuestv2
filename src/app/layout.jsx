7d55a8f5e7a2acc944f2fe3f95282eee4dcedc10
Osikoya17 <osikoyaolaoluwa2021@gmail.com>
2023-08-01 14:38:25 +0100

    testing

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -21,15 +21,16 @@ export default function RootLayout({ children }) {
           </div>
         </nav>
         {children}
-          <footer className='bg-[#0042EC] p-20  w-full h-64'>
+          <footer className='bg-[#0042EC] p-20  w-full h-full'>
             <section className='flex justify-between text-white'>
-            <div className=''>
+            <div className='basis-1/4'>
               <h2 className='text-2xl font-semibold'>NestQuest</h2>
               <p className='mt-2 text-sm'>Unlock Your Dream Home: The Trusted Choice for House Rentals</p>
 
             </div>
-            <div className='bg-slate-700'>
-              
+            <div className='basis-1/2'>
+              <h2 className='text-2xl text-center font-semibold'>Connect with us: Your Journey Starts Here!</h2>
+              <Button type='text' className='border border-l-white'>Contact Us</Button>
             </div>
             </section>
           </footer>
